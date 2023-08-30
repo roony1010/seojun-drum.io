e512f2df2b0e2f8618057bb9a9e6fc4e8b15c208
roony1010 <137221361+roony1010@users.noreply.github.com>
2023-08-30 13:55:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-document.addEventListener('keydown', playSound);
-
-function playSound(event) {
-  const keyCode = event.keyCode;
-  const audioElement = document.querySelector(`audio[data-key="${keyCode}"]`);
-  const drumPad = document.querySelector(`.drum-pad[data-key="${keyCode}"]`);
-  
-  if (audioElement) {
-    audioElement.currentTime = 0; // Reset audio to the beginning
-    audioElement.play();
-    drumPad.classList.add('playing');
-  }
-}
+// Import Howler.js library - Make sure to include the Howler.js script in your HTML
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.3/howler.min.js"></script>
 
 const drumPads = document.querySelectorAll('.drum-pad');
-drumPads.forEach(drumPad => {
-  drumPad.addEventListener('transitionend', removeTransition);
-  drumPad.addEventListener('click', function () {
-    const keyCode = drumPad.getAttribute('data-key');
-    const audioElement = document.querySelector(`audio[data-key="${keyCode}"]`);
 
-    if (audioElement) {
-      audioElement.currentTime = 0; // Reset audio to the beginning
-      audioElement.play();
-      drumPad.classList.add('playing');
-    }
+drumPads.forEach(drumPad => {
+  drumPad.addEventListener('click', function () {
+    const soundIndex = drumPad.getAttribute('data-key');
+    playSound(soundIndex);
+    drumPad.classList.add('playing');
   });
 });
 
+// Handle touch events for mobile devices
+drumPads.forEach(drumPad => {
+  drumPad.addEventListener('touchstart', function (event) {
+    event.preventDefault(); // Prevent default touch behavior
+    const soundIndex = drumPad.getAttribute('data-key');
+    playSound(soundIndex);
+    drumPad.classList.add('playing');
+  });
+});
+
+function playSound(index) {
+  // Replace with actual audio playback code for your app
+  // You can use the Web Audio API or other libraries for this purpose
+  // Example: Use the Howler.js library
+  const sound = new Howl({
+    src: [`sounds/${index}.wav`],
+  });
+
+  sound.play();
+}
+
+// Remove the 'playing' class after transition
 function removeTransition(event) {
   if (event.propertyName !== 'transform') return;
   this.classList.remove('playing');
 }
+
+drumPads.forEach(drumPad => {
+  drumPad.addEventListener('transitionend', removeTransition);
+});
